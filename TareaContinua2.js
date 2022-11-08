2a03a5c141c8f9080967b3626e7f007be530764c
Hristo <hsg2001@hotmail.com>
2022-11-08 09:07:30 +0100

    TareaContinua2

---
+++ TareaContinua2.js
@@ -0,0 +1,10 @@
+let arrayContacto=[
+{nombre:'Maxwell Wright',telefono:'(0191)7196495',correo:'Curabitur.egestas.nunc@nonumyac.co.uk'},
+{nombre:'Raja Vllareal',telefono:'0866 398 2895',correo:'posure.vulputate@sed.com'},
+{nombre:'Helen Richards',telefono:'0800 1111',correo:'libero@convallis.edu'},
+];
+    
+arrayContacto.push({nombre:'Maisie Haley',telefono:'0913 531 3030',correo:'risus.Quisque@urna.ca'});
+    
+console.log(arrayContacto[0]);
+console.log(arrayContacto[arrayContacto.length-1]);
